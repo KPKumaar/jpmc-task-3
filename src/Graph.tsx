ee41692dc180bf44872231afdc7043121610313e
KPKumaar <82832123+KPKumaar@users.noreply.github.com>
2022-10-08 16:36:07 +0530

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -48,7 +48,7 @@ class Graph extends Component<IProps, {}> {
       timestamp: 'distinct count',
       upper_bound:'avg',
       lower_bound:'avg',
-      trigger_alert:'avg'
+      trigger_alert:'avg',
       }));
     }
   }
